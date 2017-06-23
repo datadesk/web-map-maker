3379dc657917299f70a9a032a07354eb2dfb8813
Jon Schleuss <gaufre@gmail.com>
2017-06-23 09:52:12 -0700

    fix chrome not working for svg export

--- js/svg-export.js
+++ js/svg-export.js
@@ -316,8 +316,9 @@ function makeSVGCall(newMap) {
         function tileLoop() {
             setTimeout(function() {
                 var tileData;
+                console.log(tileURLs[i]);
+
                 $.get(tileURLs[i], function(data) {
-                    console.log(data);
                     tileData = data;
                 })
                 .done(function() {
@@ -720,13 +721,27 @@ function writeSVGFile(mapObject) {
                 //     console.log('yess svg is there')
                 // })
 
-                // resolve(d3.select('#export-container').html());
+                resolve(d3.select('#export-container').html());
 
     //         //jsdom done function done
     //         }
     //     })
     })
 } // writeSVG()
+
+var saveData = (function () {
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    return function (data, fileName) {
+        var blob = new Blob([data], {type: "octet/stream"}),
+            url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    };
+}());
 
 function createVector(options){
     console.log("NEW");
@@ -738,40 +753,7 @@ function createVector(options){
         .then(writeSVGFile)
         .then((svgString) => {
 
-
-
-
-// var textFile = null,
-  // makeTextFile = function (text) {
-    var data = new Blob(encodeURIComponent(svgString), {type: 'text/plain'});
-
-    // If we are replacing a previously generated file we need to
-    // manually revoke the object URL to avoid memory leaks.
-    if (textFile !== null) {
-      window.URL.revokeObjectURL(textFile);
-    }
-
-    console.log('hello')
-    textFile = window.URL.createObjectURL(data);
-
-    // return textFile;
-  // };
-
-
-//             var element = document.createElement('a');
-//             element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(svgString));
-//             element.setAttribute('download', filename);
-
-//             element.style.display = 'none';
-//             document.body.appendChild(element);
-
-//             element.click();
-
-//             document.body.removeChild(element);
-
-
-// jQuery('body').prepend(jQuery('<a/>').attr('href','data:text/octet-stream;base64,SGVsbG8gV29ybGQh').text('Click to download'))
-
+        saveData(svgString, 'map-' + getDatetime() + '.svg');
         
         });
     });
