adada6def5c8c55ea484e12d65bcc6bba374611a
Jon Schleuss <gaufre@gmail.com>
2017-07-19 16:17:46 -0700

    needed file

---
+++ js/config.js
@@ -0,0 +1,7 @@
+var configOptions = {
+    'attribution': 'YOUR NAME, ',
+    'initCoords': [34.0425, -118.24],
+    'mapzen-api': 'mapzen-neQLYXF',
+    'columnWidth': 110.45,
+    'gutterWidth': 12
+}
