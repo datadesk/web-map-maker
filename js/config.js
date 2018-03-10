6e766058eba45c9ebd8d84a3929f6736e475e542
Jon Schleuss <gaufre@gmail.com>
2018-03-10 07:51:33 -0600

    add key

---
+++ js/config.js
@@ -0,0 +1,8 @@
+var configOptions = {
+    'bingAPI': '',
+    'attribution': '@YOUR ORG, ',
+    'initCoords': [34.0425, -118.24],
+    'mapzen-api': 'O-JF_PGETeGYMBOx8xd1FA',
+    'columnWidth': 110.45,
+    'gutterWidth': 12
+}
