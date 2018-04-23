1064c1a58ada76674a3a05795f800ba64bad0e41
Jon Schleuss <gaufre@gmail.com>
2018-04-23 15:55:48 -0700

    Delete config.js

--- js/config.js
+++
@@ -1,7 +0,0 @@
-var configOptions = {
-    'attribution': '@YOUR ORG, ',
-    'initCoords': [34.0425, -118.24],
-    'mapzen-api': 'O-JF_PGETeGYMBOx8xd1FA',
-    'columnWidth': 110.45,
-    'gutterWidth': 12
-}
